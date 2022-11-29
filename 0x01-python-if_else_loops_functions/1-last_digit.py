7a68a7ca42b560f09be2546e283bc626a5fc76e5
Davies Okon <daviesaniefiok32@gmail.com>
2022-11-29 10:36:39 -0800

    fixed errors relating to negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-res = number % 10
+if number >= 0:
+    res = number % 10
+else:
+    res = last_digit = ((number * -1) % 10) * -1
 if res == 0:
     print(f"Last digit of {number:d} is {res:d} and is {res:d}")
 elif res < 6 and res != 0:
