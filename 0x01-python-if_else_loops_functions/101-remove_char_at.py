08daa8cf5e513d85a96b11243cf6e34fe743b159
Davies Okon <daviesaniefiok32@gmail.com>
2022-11-30 14:15:03 -0800

    added task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    result = str[:n] + str[n + 1:]
+    if n < 0 or n > len(str):
+        result = str
+    else:
+        result = str[:n] + str[n + 1:]
     return result
