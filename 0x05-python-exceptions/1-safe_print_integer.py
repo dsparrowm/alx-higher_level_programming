da5f7b6a6a35312e6304ff5455a774ddcbeb675a
Davies Aniefiok <daviesaniefiok32@gmail.com>
2022-12-19 12:40:46 -0800

    added task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return(True)
+        return True
     except ValueError:
-        return(False)
+        return False
+    except TypeError:
+        return False
