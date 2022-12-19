9bbb9da4c8eed24d6f5b34a45efdaa487706ee4f
Davies Aniefiok <daviesaniefiok32@gmail.com>
2022-12-19 13:12:30 -0800

    added task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             num += 1
         except TypeError:
             continue
-        except IndexError:
-            break
+        except ValueError:
+            continue
     print()
     return num
