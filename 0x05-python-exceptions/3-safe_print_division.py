c2194b7c2f7f749911b2664e70593313ccab8524
Davies Aniefiok <daviesaniefiok32@gmail.com>
2022-12-19 13:30:46 -0800

    added task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        return result
     except ZeroDivisionError:
-        return None
+        result = None
     finally:
         print("Inside result: {}".format(result))
+    return result
 
