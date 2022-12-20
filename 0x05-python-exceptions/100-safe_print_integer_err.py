e387d704c651e55180694a2da442e789f2da43c0
Davies Aniefiok <daviesaniefiok32@gmail.com>
2022-12-19 16:12:58 -0800

    added task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
+        return (True)
     except (ValueError, TypeError):
         print("Exception: {}".format(sys.exc_info()[1], file=sys.stderr))
-        return False
+        return (False)
