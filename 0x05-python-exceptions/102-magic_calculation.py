22b0a6e80fffa0d34a0aa62ddeb6f9a5d784ec82
Davies Aniefiok <daviesaniefiok32@gmail.com>
2022-12-20 04:38:13 -0800

    added magic_calculation

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception("Too far")
             else:
                 num += a ** b / i
-        except:
+        except Exception:
             num = b + a
             break
     return num
