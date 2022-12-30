8cf2aeca25e1aceed80163a45e29e7d55193b7e8
Davies Aniefiok <daviesaniefiok32@gmail.com>
2022-12-30 15:49:57 -0800

    added task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
