9c9099640de4edb79aaf673bcf6e6aa7cc6aa869
Davies Aniefiok <daviesaniefiok32@gmail.com>
2022-12-30 15:16:56 -0800

    fixed whitespace in blankline

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """defines a square by:
     Private instance attribute: size
-    Instantiation with size 
+    Instantiation with size
     (no type/value verification)"""
 
 
 class Square:
     """defines a square and initialize with size
-       
+
        args:
             size(int): size of the square
     """
