826fb332791a7d94b5b5b25419da278a9e6f62b8
Davies Aniefiok <daviesaniefiok32@gmail.com>
2022-12-30 16:19:51 -0800

    added task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,11 +9,11 @@ class Square:
     Returns:
         area of the square"""
     def __init__(self, size):
-        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
         """returns the area of a square"""
