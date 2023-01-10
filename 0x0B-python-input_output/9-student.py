d57d149f5a01182de8a288dbeca99d6c6f86571f
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-01-10 14:40:45 -0800

    added task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module creates a class that defines a student"""
+
+
+class Student:
+    """This class defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.firstname = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """returns the dictionary representation of the student"""
+        return self.__dict__
