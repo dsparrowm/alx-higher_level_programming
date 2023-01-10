4eda2eaa76968aa5894ffe2c9a07a62dc7c2e952
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-01-10 13:56:36 -0800

    fixed indentation issue

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """creates an object from a json file"""
-    witn open(filename, "r") as f:
+    with open(filename) as f:
         return json.load(f)
