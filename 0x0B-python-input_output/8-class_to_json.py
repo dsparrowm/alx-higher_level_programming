c274b65e6096471a82b39292885f63b9cbbf0dad
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-01-10 14:28:47 -0800

    added task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object:"""
+"""This module returns a dictionary desc kf simple data structure"""
 
 
 def class_to_json(obj):
-    """function that returns the dict description with simple data"""
+    """function that returns the dict desc with simple data"""
     return obj.__dict__
