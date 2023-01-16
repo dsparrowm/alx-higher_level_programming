91ce438fbe7a6f18fd2750ffee040d912feeefb3
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-01-16 12:52:16 -0800

    added some methods in Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,6 +30,54 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
+    def from_json_string(json_string):
+        """return the deserialization of a JSON string"""
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return a class instantied from a dictionary
+        of attributes."""
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+                new.update(**dictionary)
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of classes instantiated from a file
+        of JSON strings."""
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Write the CSV serialization of a list of objects
+        to a file."""
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline="") as csvfile:
+            if list_objs is None or list_objs == []:
+                csvfile.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        for obj in list_objs:
+            writer.writerow(obj.to_dictionary())
+
     @classmethod
     def safe_to_file(cls, list_objs):
         """write a list of objects to a file"""
