6fc9e58db0463243ce682bf97fb14196f3d88712
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-03-13 20:00:31 +0000

    fixed errors

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 exports.callMeMoby = function (x, theFunction) {
-  let i = 0;
-  while (i < x) {
-    theFunction();
-    i++;
-  }
+  for (let i = 0; i < x; i++) theFunction();
 }
