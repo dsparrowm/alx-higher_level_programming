38e1fa85816d60e989703227a9cfdd1aa5bc3e2b
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-03-13 17:37:18 +0000

    added task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const num1 = Number(process.argv[2]);
 const num2 = Number(process.argv[3]);
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 console.log(add(num1, num2));
