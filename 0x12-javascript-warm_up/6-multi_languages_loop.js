1137f3686838abf7b67c38fd19528897177fdd49
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-03-13 16:35:22 +0000

    fixed errors

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 while ((i < myArr.length)) {
   console.log(myArr[i]);
