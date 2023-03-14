221e6b9a8d41e4c75c95385e0d17f38439a4b3ad
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-03-14 12:41:34 +0000

    added task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,8 +5,7 @@ module.exports = class Rectangle {
       this.width = w;
       this.height = h;
     }
-  }
-  
+  }  
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
