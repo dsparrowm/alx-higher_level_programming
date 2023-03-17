c7333f561d1fccdc3300443f930606f38b956ae5
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-03-17 12:33:35 -0700

    added task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa:
+"""
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states WHERE name LIKE "N%"')
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+    cursor.close()
+    db.close()
