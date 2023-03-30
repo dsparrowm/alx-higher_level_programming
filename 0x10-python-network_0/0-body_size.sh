6eb762ab98aec0d260dade7a594c44b83a8cc3c2
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-03-30 14:08:15 -0700

    added task 0 and Readme

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
 # A script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-size=$(curl -sI "$1" | awk '/Content-Length/ {print $2}')
-echo ${size}
+curl -s "$1" | wc -c
