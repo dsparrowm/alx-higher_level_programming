2ebf97a894cf7c212804c0dc76642549fc389fcf
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-03-31 07:19:37 -0700

    added Readme and task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+url = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    print("Body response:")
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content : {}'.format(html.decode('utf-8')))
