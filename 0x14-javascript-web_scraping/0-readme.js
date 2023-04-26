f32d7471e1cd25086a758d928749f346c96c0217
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-04-26 09:26:41 +0000

    fixed errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,10 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', function (err, data) {
-  console.log(err);
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err);
+} else {
   console.log(data);
+}
 });
